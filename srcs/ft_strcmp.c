2ceb47671a8a3349646e16f80d1f963cb1ebc787
Mike Sargsyan <misargsy@c1r24s10.42tokyo.jp>
2023-11-13 17:45:49 +0900

    fixed strcmp conflicting types

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,13 +6,13 @@
 /*   By: misargsy <misargsy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 19:11:12 by misargsy          #+#    #+#             */
-/*   Updated: 2023/11/11 19:12:41 by misargsy         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:45:06 by misargsy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(char *str1, char *str2)
+int	ft_strcmp(const char *str1, const char *str2)
 {
 	while (((*str1 != '\0')) && (*str2 != '\0'))
 	{
